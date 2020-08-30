9feb51491f2985619ed1f60fcfb74662c5b9f69c
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-08-31 00:53:11 +0900

    feat:タイトルとファビコンの追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,6 +22,7 @@ export default ({ titlePre = '' }) => {
           name="description"
           content="An example Next.js site using Notion for the blog"
         />
+        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
         <meta name="og:title" content="My Notion Blog" />
         <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:site" content="@_ijjk" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default () => (
   <>
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
-      <h1>キネマトグラフ</h1>
+      <h1>&#x1f4fd;キネマトグラフ</h1>
       <img src="/eltopo.jpeg" alt="" />
       <div className="explanation">
         <p>鑑賞した映画をまとめていく</p>
