f92fa940a1ba68e96c73d4f228a18a851adf1785
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-09-02 00:34:36 +0900

    feat:ブログページをindexページにする

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,9 @@
-import Link from 'next/link'
 import Head from 'next/head'
-import ExtLink from './ext-link'
-import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
-
-const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'ブログ', page: '/blog' },
-]
 
 const ogImageUrl = '../../public/eltopo.jpeg'
 
 export default ({ titlePre = '' }) => {
-  const { pathname } = useRouter()
-
   return (
     <header className={styles.header}>
       <Head>
@@ -31,21 +22,6 @@ export default ({ titlePre = '' }) => {
       <a href="/">
         <h1>&#x1f4fd;キネマトグラフ</h1>
       </a>
-      <ul>
-        {navItems.map(({ label, page, link }) => (
-          <li key={label}>
-            {page ? (
-              <Link href={page}>
-                <a className={pathname === page ? 'active' : undefined}>
-                  {label}
-                </a>
-              </Link>
-            ) : (
-              <ExtLink href={link}>{label}</ExtLink>
-            )}
-          </li>
-        ))}
-      </ul>
     </header>
   )
 }
